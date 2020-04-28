ad471fd9a61255628cb50f43415460464bafe66d
Thomas Schmelzer <thomas.schmlezer@gmail.com>
2020-04-28 17:32:32 +0200

    remove read_pd

--- test/config.py
+++ test/config.py
@@ -6,15 +6,11 @@ def resource(name):
     return os.path.join(os.path.dirname(__file__), "resources", name)
 
 
-def read_pd(name, **kwargs):
-    return pd.read_csv(resource(name), **kwargs)
-
-
 def get_data():
     # https://github.com/robertmartin8/PyPortfolioOpt
-    return read_pd("stock_prices.csv", parse_dates=True, index_col="date")
+    return pd.read_csv(resource("stock_prices.csv"), parse_dates=True, index_col="date")
 
 
 def get_benchmark_data():
     # https://github.com/robertmartin8/PyPortfolioOpt
-    return read_pd("spy_prices.csv", parse_dates=True, index_col="date")
+    return pd.read_csv(resource("spy_prices.csv"), parse_dates=True, index_col="date")
