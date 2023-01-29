3473c3eaa35d92800a89c3e18e98c8a56a053745
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-01-29 20:15:53 +0400

    Update graph.py

--- pyhrp/graph.py
+++ pyhrp/graph.py
@@ -1,9 +1,10 @@
-# pylint: disable=invalid-name, missing-module-docstring, missing-function-docstring
+"""display a dendrogram"""
 import matplotlib.pyplot as plt
 import scipy.cluster.hierarchy as sch
 
 
 def dendrogram(links, ax=None, **kwargs):
+    """Plot a dendrogram using matplotlib"""
     if ax is None:
         _, ax = plt.subplots(figsize=(25, 20))
     sch.dendrogram(links, ax=ax, **kwargs)
