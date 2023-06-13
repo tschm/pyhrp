ae73b68d69851626078e0f9d3f02fe797ca201f1
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-06-12 23:16:47 -0700

    Update conf.py

--- book/sphinx/conf.py
+++ book/sphinx/conf.py
@@ -38,9 +38,9 @@ napoleon_numpy_docstring = True
 # html_logo = "_static/logo.png"
 html_title = "pyhrp Documentation"
 html_show_sourcelink = True
-html_css_files = [
-    "custom.css",
-]
+#html_css_files = [
+#    "custom.css",
+#]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
@@ -49,4 +49,4 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
