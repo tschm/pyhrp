e2a7da8862b3f14549b0da68fae8ee3302e29124
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-09 21:15:06 +0400

    228 remove node class2 (#237)
    * bring in binarytree
    
    * testing
    
    * using len in Node
    
    * cutting down Node
    
    * convoluted functions
    
    * remove binarytree

--- src/tests/test_node.py
+++ src/tests/test_node.py
@@ -1,4 +1,4 @@
-from pyhrp.hrp import Node, _bisection
+from pyhrp.hrp import Node
 
 
 def test_node():
@@ -7,8 +7,8 @@ def test_node():
 
     assert left.id == 0
     assert right.id == 1
-    assert left.count == 1
-    assert right.count == 1
+    assert len(left) == 1
+    assert len(right) == 1
 
     assert left.pre_order() == [0]
     assert right.pre_order() == [1]
@@ -21,26 +21,9 @@ def test_node():
     assert node.id == 5
     assert node.left.id == 0
     assert node.right.id == 1
-    assert node.left.count == 1
-    assert node.right.count == 1
-    assert node.count == 2
+    assert len(node.left) == 1
+    assert len(node.right) == 1
+    assert len(node) == 2
     assert node.pre_order() == [0, 1]
 
     assert not node.is_leaf()
-
-    root = _bisection(ids=[0, 1], n=2)
-    assert isinstance(root, Node)
-    assert root.id == 2
-    assert root.left.id == 0
-    assert root.right.id == 1
-    assert root.pre_order() == [0, 1]
-
-
-def test_bisection():
-    root = _bisection(ids=[2, 3, 1, 0, 4, 6, 5, 7], n=8)
-    assert root.pre_order() == [2, 3, 1, 0, 4, 6, 5, 7]
-
-
-def test_repr():
-    left = Node(id=0)
-    assert repr(left) == "Node(id=0, left=None, right=None)"
