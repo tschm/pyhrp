ad9dcb24dcc474025eedda805c3fd7a82df14cfa
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-09 20:12:28 +0400

    testing

--- src/tests/test_node.py
+++ src/tests/test_node.py
@@ -1,4 +1,4 @@
-from pyhrp.hrp import Node, _bisection
+from pyhrp.hrp import Node
 
 
 def test_node():
@@ -27,20 +27,3 @@ def test_node():
     assert node.pre_order() == [0, 1]
 
     assert not node.is_leaf()
-
-    root = _bisection(ids=[0, 1], n=2)
-    assert isinstance(root, Node)
-    assert root.id == 2
-    assert root.left.id == 0
-    assert root.right.id == 1
-    assert root.pre_order() == [0, 1]
-
-
-def test_bisection():
-    root = _bisection(ids=[2, 3, 1, 0, 4, 6, 5, 7], n=8)
-    assert root.pre_order() == [2, 3, 1, 0, 4, 6, 5, 7]
-
-
-def test_repr():
-    left = Node(id=0)
-    assert repr(left) == "Node(id=0, left=None, right=None)"
