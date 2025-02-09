2bf1d30e9d39448659455785fe18674683ffa11e
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-09 12:02:53 +0400

    marimo demo

--- book/marimo/demo.py
+++ book/marimo/demo.py
@@ -1,23 +1,81 @@
 import marimo
 
-__generated_with = "0.9.27"
+__generated_with = "0.11.0"
 app = marimo.App()
 
 
 @app.cell
-def __(mo):
+def _(mo):
     mo.md(r"""# Demo pyhrp""")
     return
 
 
 @app.cell
-def __():
-    1 + 2
+def _(__file__):
+    from pathlib import Path
+
+    import pandas as pd
+
+    path = Path(__file__).parent
+    prices = pd.read_csv(path / "data" / "stock_prices.csv", index_col=0)
+
+    # compute returns
+    returns = prices.pct_change().dropna(axis=0, how="all").fillna(0.0)
+    return Path, path, pd, prices, returns
+
+
+@app.cell
+def _(returns):
+    # compute the dendrogram
+    from pyhrp.graph import dendrogram
+    from pyhrp.hrp import _dist, linkage
+
+    cor = returns.corr().values
+    links = linkage(_dist(cor), method="ward")
+    dendrogram(links=links, labels=returns.columns)
+    return cor, dendrogram, _dist, linkage, links
+
+
+@app.cell
+def _(links):
+    from pyhrp.hrp import tree
+
+    rootnode = tree(links)
+    # moving from the rootnode to the left we end up on a node
+    print(rootnode.get_left())
+    # moving from the rootnode to the right we end up on a node
+    print(rootnode.get_right())
+    print(rootnode.get_count())
+    print(rootnode.get_left().get_count())
+    print(rootnode.get_right().get_count())
+    return rootnode, tree
+
+
+@app.cell
+def _(rootnode):
+    rootnode.pre_order()
+    return
+
+
+app._unparsable_cell(
+    r"""
+    rootnode.get_id()
+    rootnode.get_left().get_id()
+    rootnode.get_right().get_id()
+    rootnode.
+    """,
+    name="_",
+)
+
+
+@app.cell
+def _(links):
+    links
     return
 
 
 @app.cell
-def __():
+def _():
     import marimo as mo
 
     return (mo,)
