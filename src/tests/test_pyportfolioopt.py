09fd3f89e98c48fa3e678a28e30869d43ae9aa34
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-09 14:36:16 +0400

    README updates

---
+++ src/tests/test_pyportfolioopt.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+from pypfopt import HRPOpt
+
+from pyhrp.cluster import hrp
+
+
+def test_allocation(prices):
+    returns = prices.pct_change().dropna(axis=0, how="all")
+
+    optimizer = HRPOpt(returns)
+    weights = optimizer.optimize(linkage_method="single")
+    ww = pd.Series(weights)
+
+    cluster = hrp(prices=prices, method="single", bisection=True)
+    print(cluster.weights)
+
+    assert np.linalg.norm(ww - cluster.weights) < 0.006
