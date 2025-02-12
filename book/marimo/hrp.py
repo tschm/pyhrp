7549cccce918b4809847380ff045dc8fcfc1c3b0
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-12 21:02:22 +0400

    notebook for hrp

--- book/marimo/hrp.py
+++ book/marimo/hrp.py
@@ -6,7 +6,18 @@ app = marimo.App()
 
 @app.cell
 def _(mo):
-    mo.md(r"""# Demo pyhrp""")
+    mo.md(
+        r"""
+        # Hierarchical Risk Parity (HRP)
+
+        We follow ideas by Marcos Lopez de Prado.
+
+        - Compute the 1st dendrogram using the 'single' distance method.
+        - Compute the 2nd dendrogram by using the order of leaves
+          of the 1st dendrogram (following an argument by Thomas Raffinot)
+        - Apply Risk Parity in a recursive bottom-up traverse
+        """
+    )
     return
 
 
@@ -35,32 +46,119 @@ def _(returns):
 
 
 @app.cell
-def _(cor):
+def _():
     # The implementation by Marcos Lopez de Prado is based on the 'single' metric
+    import matplotlib.pyplot as plt
+
+    from pyhrp.algos import risk_parity
     from pyhrp.hrp import build_tree
 
-    dendrogram_before = build_tree(cor, method="single")
-    dendrogram_before.plot()
-    return build_tree, dendrogram_before
+    return build_tree, plt, risk_parity
 
 
 @app.cell
-def _(build_tree, cor):
+def _(build_tree, cor, cov):
+    # The first dendrogram is suffering. We observe the chaining effect
+    dendrogram_before = build_tree(cor, method="single")
+    dendrogram_before.plot(labels=cov.columns)
+    return (dendrogram_before,)
+
+
+@app.cell
+def _(cov, dendrogram_before, plt, risk_parity):
+    # The weights are not well balanced
+    # No surprise given exposure of nodes like 11, 12 or 15
+    root_before = risk_parity(dendrogram_before.root, cov)
+    weights_before = root_before.portfolio.weights
+
+    weights_before.plot(kind="bar")  # Or another plot type if needed
+
+    # Ensure all possible x-axis labels are shown
+    plt.xticks(ticks=range(len(weights_before)), labels=weights_before.index, rotation=90)
+
+    # Optionally, adjust the layout to avoid label clipping
+    plt.tight_layout()
+
+    plt.show()
+
+    return root_before, weights_before
+
+
+@app.cell
+def _(build_tree, cor, cov):
     # The dendrogram suffers because of the 'chaining' effect. LdP is using
     # now only the order of the leaves (e.g. the assets) and
     # constructs a second Dendrogram.
     dendrogram_bisection = build_tree(cor, method="single", bisection=True)
-    dendrogram_bisection.plot()
+    dendrogram_bisection.plot(labels=cov.columns)
     return (dendrogram_bisection,)
 
 
 @app.cell
-def _(cov, dendrogram_bisection):
-    from pyhrp.algos import risk_parity
+def _(cov, dendrogram_bisection, plt, risk_parity):
     # root = dendrogram_bisection.root
 
-    root = risk_parity(dendrogram_bisection.root, cov)
-    return (root,)
+    root_bisection = risk_parity(dendrogram_bisection.root, cov)
+
+    weights_bisection = root_bisection.portfolio.weights
+
+    weights_bisection.plot(kind="bar")  # Or another plot type if needed
+
+    # Ensure all possible x-axis labels are shown
+    plt.xticks(ticks=range(len(weights_bisection)), labels=weights_bisection.index, rotation=90)
+
+    # Optionally, adjust the layout to avoid label clipping
+    plt.tight_layout()
+
+    plt.show()
+    return root_bisection, weights_bisection
+
+
+@app.cell
+def _(build_tree, cor, cov):
+    dendrogram_ward = build_tree(cor, method="ward")
+    dendrogram_ward.plot(labels=cov.columns)
+    return (dendrogram_ward,)
+
+
+@app.cell
+def _(cov, dendrogram_ward, plt, risk_parity, weights_bisection):
+    root_ward = risk_parity(dendrogram_ward.root, cov)
+    weights_ward = root_ward.portfolio.weights
+
+    weights_ward.plot(kind="bar")  # Or another plot type if needed
+
+    # Ensure all possible x-axis labels are shown
+    plt.xticks(ticks=range(len(weights_ward)), labels=weights_bisection.index, rotation=90)
+
+    # Optionally, adjust the layout to avoid label clipping
+    plt.tight_layout()
+
+    plt.show()
+    return root_ward, weights_ward
+
+
+@app.cell
+def _(pd, plt, root_bisection, root_ward):
+    # Assuming root_before.portfolio.weights1 and root_before.portfolio.weights2 are two weight series
+    _weights_ward = root_ward.portfolio.weights
+    _weights_bisection = root_bisection.portfolio.weights
+
+    # Create a DataFrame from both weight series to align them on the same index (assuming they have the same index)
+    weights_df = pd.DataFrame({"ward": _weights_ward, "single/bisection": _weights_bisection})
+
+    # Plot both weight series
+    weights_df.plot(kind="bar", width=0.8)
+
+    # Ensure all possible x-axis labels are shown
+    plt.xticks(ticks=range(len(weights_df)), labels=weights_df.index, rotation=90)
+
+    # Optionally, adjust the layout to avoid label clipping
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
+    return (weights_df,)
 
 
 @app.cell
