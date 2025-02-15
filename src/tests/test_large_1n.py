249ce1b3b0a49ca1d4ce0fd10a61d30d7f466ffa
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-15 10:36:33 +0400

    testing larger problem

---
+++ src/tests/test_large_1n.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+
+from pyhrp.algos import one_over_n
+from pyhrp.hrp import build_tree
+
+
+def test_one_over_n(returns):
+    cor = returns.corr()
+    dendrogram = build_tree(cor=cor, method="ward")
+    dendrogram.plot()
+    plt.show()
+
+    for level, portfolio in one_over_n(dendrogram).items():
+        print(f"Level: {level}")
+        print(portfolio)
