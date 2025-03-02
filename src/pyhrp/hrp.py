fixing computation of linkage

src/pyhrp/hrp.py
@@ -53,6 +53,8 @@ class Dendrogram:
     def plot(self, **kwargs):
         """Plot the dendrogram"""
         labels = [asset.name for asset in self.assets]
+        print(self.linkage)
+
         sch.dendrogram(self.linkage, leaf_rotation=90, leaf_font_size=8, labels=labels, **kwargs)
 
     @property
@@ -133,7 +135,14 @@ def build_tree(
             if node.left is not None:
                 get_linkage(node.left)
                 get_linkage(node.right)
-                links.append([node.left.value, node.right.value, float(node.size), node.size])
+                links.append(
+                    [
+                        node.left.value,
+                        node.right.value,
+                        float(node.size),
+                        len(node.left.leaves) + len(node.right.leaves),
+                    ]
+                )
 
         get_linkage(root)
         links = np.array(links)